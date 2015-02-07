a8bdd6b2502af7304af32dce4a13dfd94d159a05
Natalia Stanko <anithaly@gmail.com>
2015-02-07 21:25:07 +0100

    add voyage detail view

--- voyage/urls.py
+++ voyage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.voyage_list),
+    url(r'^(?P<pk>[0-9]+)/$', views.voyage_detail)
 )
--- voyage/views.py
+++ voyage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Place
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
 def voyage_list(request):
     places = Place.objects.filter(published_date__isnull=False).order_by('published_date')
     return render(request, 'voyage/voyage_list.html', {'places': places})
+
+def voyage_detail(request, pk):
+    # place = get_object_or_404(Place, pk=pk)
+    place = Place.objects.get(pk=pk)
+    return render(request, 'voyage/voyage_detail.html', {'place': place})
