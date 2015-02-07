c5fcbd3fa16f5af34a328fcde1d86bc66e043811
Natalia Stanko <anithaly@gmail.com>
2015-02-07 20:22:59 +0100

    voyages list

--- voyage/views.py
+++ voyage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
+def voyage_list(request):
+    places = Place.objects.filter(published_date__isnull=False).order_by('published_date')
+    return render(request, 'voyage/voyage_list.html', {'places': places})
