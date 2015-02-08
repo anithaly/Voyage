a93b0111c1496710db9a93d6a53f666a38cf2f91
Natalia Stanko <anithaly@gmail.com>
2015-02-08 20:27:12 +0100

    structure cleaning

--- voyage/urls.py
+++ voyage/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
-#voyage/
+#place/
 urlpatterns = patterns('',
-    url(r'^$', views.voyage_list),
-    url(r'^(?P<pk>[0-9]+)/$', views.voyage_detail, name='voyage_list'),
-    url(r'^new$', views.voyage_new, name='voyage_new'),
-    url(r'^(?P<pk>[0-9]+)/edit$', views.voyage_edit, name='voyage_edit'),
-    url(r'^(?P<pk>[0-9]+)/publish$', views.voyage_publish, name='voyage_publish'),
-    url(r'^drafts$', views.voyage_draft_list, name='voyage_draft_list'),
-    url(r'^(?P<pk>[0-9]+)/remove$', views.voyage_remove, name='voyage_remove'),
-    url(r'^(?P<pk>[0-9]+)/comment$', views.voyage_comment_new, name='voyage_comment_new'),
+    url(r'^$', views.place_list),
+    url(r'^(?P<pk>[0-9]+)/$', views.place_detail, name='place_list'),
+    url(r'^new$', views.place_new, name='place_new'),
+    url(r'^(?P<pk>[0-9]+)/edit$', views.place_edit, name='place_edit'),
+    url(r'^(?P<pk>[0-9]+)/publish$', views.place_publish, name='place_publish'),
+    url(r'^drafts$', views.place_drafts_list, name='place_drafts_list'),
+    url(r'^(?P<pk>[0-9]+)/remove$', views.place_remove, name='place_remove'),
+    url(r'^(?P<pk>[0-9]+)/comment$', views.place_comment_new, name='place_comment_new'),
 )
--- voyage/views.py
+++ voyage/views.py
@@ -7,31 +7,31 @@ from .models import Place, Comment
 from .forms import PlaceForm, CommentForm
 
 # @login_required
-def voyage_list(request):
+def place_list(request):
     places = Place.objects.filter(published_date__isnull=False).order_by('-published_date')
-    return render(request, 'voyage/voyage_list.html', {'places': places})
+    return render(request, 'place/list.html', {'places': places})
 
-def voyage_detail(request, pk):
+def place_detail(request, pk):
     place = get_object_or_404(Place, pk=pk)
     form = CommentForm(request.POST or None)
     # place = Place.objects.get(pk=pk)
-    return render(request, 'voyage/voyage_detail.html', {'place': place, 'form': form})
+    return render(request, 'place/detail.html', {'place': place, 'form': form})
 
 @login_required
-def voyage_new(request):
+def place_new(request):
     if request.method == "POST":
         form = PlaceForm(request.POST)
         if form.is_valid():
             place = form.save(commit=False)
             place.author = request.user
             place.save()
-            return redirect('voyage.views.voyage_detail', pk=place.pk)
+            return redirect('voyage.views.place_detail', pk=place.pk)
     else:
         form = PlaceForm()
-    return render(request, 'voyage/voyage_form.html', {'form': form})
+    return render(request, 'place/form.html', {'form': form})
 
 @login_required
-def voyage_edit(request, pk):
+def place_edit(request, pk):
     place = get_object_or_404(Place, pk=pk)
     if request.method == "POST":
         form = PlaceForm(request.POST, instance=place)
@@ -39,30 +39,30 @@ def voyage_edit(request, pk):
             place = form.save(commit=False)
             place.author = request.user
             place.save()
-            return redirect('voyage.views.voyage_detail', pk=place.pk)
+            return redirect('voyage.views.place_detail', pk=place.pk)
     else:
         form = PlaceForm(instance=place)
-    return render(request, 'voyage/voyage_form.html', {'form': form, 'place': place})
+    return render(request, 'place/form.html', {'form': form, 'place': place})
 
 @login_required
-def voyage_remove(request, pk):
+def place_remove(request, pk):
     place = get_object_or_404(Place, pk=pk)
     place.delete()
-    return redirect('voyage.views.voyage_list')
+    return redirect('voyage.views.place_list')
 
 @login_required
-def voyage_publish(request, pk):
+def place_publish(request, pk):
     place = get_object_or_404(Place, pk=pk)
     place.publish()
-    return redirect('voyage.views.voyage_detail', pk=pk)
+    return redirect('voyage.views.place_detail', pk=pk)
 
 @login_required
-def voyage_draft_list(request):
-    place = Place.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'voyage/voyage_draft_list.html', {'places': places})
+def place_drafts_list(request):
+    places = Place.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'place/drafts_list.html', {'places': places})
 
 @login_required
-def voyage_comment_new(request, pk):
+def place_comment_new(request, pk):
     place = get_object_or_404(Place, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
@@ -71,7 +71,7 @@ def voyage_comment_new(request, pk):
             comment.author = request.user
             comment.place = place
             comment.save()
-            return redirect('voyage.views.voyage_detail', pk=place.pk)
+            return redirect('voyage.views.place_detail', pk=place.pk)
     else:
         form = CommentForm()
-    return render(request, 'voyage/voyage_detail.html', {'place': place, 'form': form})
+    return render(request, 'place/detail.html', {'place': place, 'form': form})
